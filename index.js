7581291a03094fe4641cf080e0343ba705b84e1e
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-05-27 03:02:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const wordsCtrl = require('./controllers/word.ctrl');
+const express = require('express');
+const userCtrl = require('./controllers/user.ctrl');
+const authCtrl = require('./controllers/auth.ctrl');
+const jwt = require('jsonwebtoken');
+const app = express();
+const PORT = 5678;
+app.use(express.json());
+app.use('/api/auth', authCtrl);
+
+app.use(function (req, res,next) {
+    try {
+        const [type, token] = req.headers.authorization.split(' ');
+        const isCorrect = jwt.verify(token, "myKey");
+        next();
+    }
+    catch(ex) {
+        res.status(401).send();
+    }
+})
+app.use('/api/word', wordsCtrl);
+app.use('/', userCtrl);
+
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
