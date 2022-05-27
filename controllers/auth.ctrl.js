a14b819b28227a3e67dd16f6aee3527cee1d818a
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-05-27 10:35:47 +0300

    Update auth.ctrl.js
    change to hash

--- controllers/auth.ctrl.js
+++ controllers/auth.ctrl.js
@@ -9,7 +9,7 @@ authCtrl.post('/login', function (req, res) {
     };
     const usersArr = require('../data/users.json').find(u => u.email == detail.email && u.password == detail.password);
     usersArr && delete usersArr['password'];
-    const jwtKey = jwt.sign(usersArr, "myKey");
+    const jwtKey = jwt.sign(usersArr, "f40c189314dafd35aab9d71a29ee32cc");
     res.type('.txt').send(jwtKey);
 
 })
